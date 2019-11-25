06842501934aad4816c02dbd73e5b538639b5f41
千夜 <1342746626@qq.com>
2019-11-26 02:09:55 +0800

    数组，切片

---
+++ src/Array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//创建空数组 var arr [3]int
+
+	//创建数组
+	arr:=[3]int{3, 4, 5} //或 arr:=[...]int{3, 4, 5} 自动判断长度
+
+	//迭代方式
+	for i:=0;i<len(arr);i++{
+		fmt.Print(arr[i])
+	}
+
+	//迭代方式 2 ， i为索引，v为值;
+	//可将 i或v 替换为 _ 表示忽略不使用
+	for i,v:=range arr{
+		fmt.Printf("索引 %d ,值 %d\n",i, v)
+	}
+
+}
