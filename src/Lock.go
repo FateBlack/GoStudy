f36bfa645acdd84ad3381522d3315ac36f4096fa
千夜 <1342746626@qq.com>
2019-11-27 01:07:27 +0800

    add lock

---
+++ src/Lock.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	num   int
+	wg    sync.WaitGroup
+	mutex sync.Mutex
+)
+
+func main() {
+	wg.Add(2)
+	go addNum("A")
+	go addNum("B")
+	wg.Wait()
+}
+
+func addNum(name string) {
+	defer wg.Done()
+
+	for i := 0; i < 100; i++ {
+		mutex.Lock()
+		num = num + 1
+		fmt.Println(name, num)
+		mutex.Unlock()
+	}
+}
