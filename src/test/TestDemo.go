5c940a55f109b475022df200064d8b888eb68a6a
千夜 <1342746626@qq.com>
2019-11-28 09:59:20 +0800

    update API测试方式

--- src/test/TestDemo.go
+++ src/test/TestDemo.go
@@ -1,6 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 //待测试函数
-func Demo(a, b int) int {
+func Add(a, b int) int {
 	return a + b
+}
+
+/*
+	处理Http请求函数
+*/
+func Routes() {
+	http.HandleFunc("/sendjson", SendJson) //设置路由
+}
+
+func SendJson(rw http.ResponseWriter, req *http.Request) {
+	u := struct {
+		Name string
+	}{
+		Name: "老王",
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK) //code码
+	json.NewEncoder(rw).Encode(u)
 }
--- src/test/TestDemo_test.go
+++ src/test/TestDemo_test.go
@@ -1,14 +1,38 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
+//注：测试文件后缀必须 _test
 //cd 指定文件目录下 go test -v 即可
 //单元测试
-func TestMethod(t *testing.T) {
-	sum := Demo(2, 3)
+func TestAdd(t *testing.T) {
+	sum := Add(2, 3)
 	if sum == 5 {
 		t.Log("成功")
 	} else {
 		t.Fatal("失败")
 	}
+}
+
+/*
+	测试 API
+*/
+func init() {
+	Routes() //初始化调用 待测试函数的 路由配置函数
+}
+func TestSendJson(t *testing.T) {
+	req, err := http.NewRequest(http.MethodGet, "/sendjson", nil)
+	if err != nil {
+		t.Fatal("创建request失败")
+	}
+
+	rw := httptest.NewRecorder()            //模拟服务端响应
+	http.DefaultServeMux.ServeHTTP(rw, req) //触发响应
+
+	t.Log("code:", rw.Code)
+	t.Log("body:", rw.Body.String())
 }
